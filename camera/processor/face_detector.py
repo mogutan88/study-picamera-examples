63dc241658d47a001077d1fc98fc4559a4160b35
Yuichi OMATA <yuichi.omata@dena.com>
2018-10-10 19:31:16 +0900

    ex3 show number of detected faces

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -41,5 +41,8 @@ class FaceDetector(object):
         for (x,y,w,h) in faces:
             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
 
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(frame, 'Faces: {}'.format(len(faces)), (10, 10), font, 2, (255,255,255))
+
         # frameを戻り値として返す
         return frame
